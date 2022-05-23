020b3c4139fdc77619f945824383d4df26b0b650
Hamed Bahrami <hi@hamedbahrami.de>
2022-05-23 20:21:15 +0200

    x

--- node_modules/ical-js-parser/interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ExtractProperty {
-    mainProperty: string;
-    extractedProperty: string;
-}
--- node_modules/ical-js-parser/toJSON/timezoneParser.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Parse different formats of timezone to recognizable output
- * @param timezone
- */
-export declare const timezoneParser: (timezone: string) => string;
